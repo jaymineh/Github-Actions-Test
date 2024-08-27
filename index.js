274a1600c847e11fe03a41da26447b749613370d
Jaymineh <91850543+jaymineh@users.noreply.github.com>
2024-08-27 16:28:30 +0000

    tete

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-// Example: index.js
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+i wan
